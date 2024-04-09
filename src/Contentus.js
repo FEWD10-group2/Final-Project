f87ddf5e6dc40cf2f6616c55e90ad67e3990e888
sing <sing09512@gmail.com>
2024-04-09 20:55:52 +0800

    Still need mobile version CSS

--- src/Contentus.js
+++ src/Contentus.js
@@ -39,9 +39,9 @@ const ContactForm = () => {
     e.target.reset();
   };
   return (
-    <div className="-mx-2 flex flex-wrap pt-8 pb-8 bg-cover bg-center justify-center items-center">
+    <div className="-mx-2 m md:w-1/2 flex flex-wrap pt-8 pb-8 bg-cover bg-center justify-center items-center">
       <div className="flex w-full md:w-1/2 px-2 flex-col">
-        <div className="overflow-hidden w-[550px] h-[380px] ml-56 mb-8 mt-32">
+        <div className="overflow-hidden w-[550px] h-[380px] ml-36 mb-4 mt-20 absolute">
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d922.447965134475!2d114.1318653694974!3d22.36148663898441!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3403f89825833649%3A0x40948ca5db317aa4!2z6JG15raM6JG15piM6LevNTHomZ_kuZ3pvo3osr_mmJPkuK3lv4Mx5bqn!5e0!3m2!1szh-TW!2shk!4v1712129789778!5m2!1szh-TW!2shk"
             class="w-full h-full border-0"
@@ -50,25 +50,28 @@ const ContactForm = () => {
             referrerPolicy="no-referrer-when-downgrade"
           ></iframe>
         </div>
-        <div class="flex w-full md:w-1/2 px-2 flex-col ml-60">
-          <div className="mb-4 flex items-center">
-            <img src={address} alt="Phone" className="w-6 h-6 mr-2" />
-            <span>Address:123</span>
+        <div class="flex flex-basic-2 md:w-1/2 px-2 flex-col ml-36 absolute top-[37rem]">
+          <div className="mb-4 flex items-center pr-[25rem]">
+            <img src={address} alt="Address" className="w-6 h-6 mr-2" />
+            <p>
+              Address: Room 8A, 8/F, Tower 1, Kowloon Commerce Centre, 51 Kwai
+              Cheong Road, Kwai Chung.
+            </p>
           </div>
-          <div className="mb-4 flex items-center">
+          <div className="mb-4 flex items-center pr-4">
             <img src={phone} alt="Phone" className="w-6 h-6 mr-2" />
-            <span>Phone:123</span>
+            <p>Phone: 21800000</p>
           </div>
           <div className="mb-4 flex items-center">
-            <img src={time} alt="Phone" className="w-6 h-6 mr-2" />
-            <span>Work Time:123</span>
+            <img src={time} alt="Time" className="w-6 h-6 mr-2" />
+            <span>Work Time: 9:30AM - 17:00PM</span>
           </div>
         </div>
       </div>
-      <div className="w-full m md:w-1/2 mt-32">
+      <div className="w-1/2 m md:w-1/4 mt-32">
         <form
           onSubmit={sendEmail}
-          className="border rounded-lg shadow-lg bg-white p-4 mr-60"
+          className="border rounded-lg shadow-lg bg-white p-4 w-[30rem] absolute top-[10.5rem] right-[25.25rem]"
         >
           <h2 className="text-2xl font-bold mb-4 text-center text-gray-800">
             Content Us
