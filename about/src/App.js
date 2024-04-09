137f306b74695c0998aa32fe72fdffdff20d43fb
jacky <jackywan68@gmail.com>
2024-04-09 14:25:43 +0800

    50%

--- about/src/App.js
+++ about/src/App.js
@@ -3,12 +3,12 @@ import "./App.css";
 function App() {
   return (
     <div>
-      <div className="bg-top-bg h-[550px] bg-cover top-0 left-0 right-0 z-10 border rounded-bl-[60px] ">
+      <div className="bg-top-bg h-[550px] w-auto bg-cover top-0 left-0 right-0 z-10 border rounded-bl-[60px] ">
         <p className="flex items-center justify-center text-white text-5xl">
           About us
         </p>
       </div>
-      <div className="border rounded-lg absolute flex justify-center flex-nowrap flex-col z-20 mx-10">
+      <div className="w-[840px] border rounded-lg absolute flex justify-center flex-nowrap flex-col z-20 px-10 ml-52 mb-10">
         <p>See The World For Less</p>
         <span className="m-0">
           Agoda is an online travel platform that brings high-value and
@@ -23,7 +23,7 @@ function App() {
           supported by a truly diverse team of more than 6,700 people in 31
           markets and Agoda customer service available 24/7.
         </span>
-        <p>Visitor Numbers</p>
+        <p className="flex items-center">Visitor Numbers</p>
         <p>
           In accordance with Agoda.com obligations under the EU’s Digital
           Services Act (DSA), we estimate that the average monthly recipients*
@@ -42,7 +42,7 @@ function App() {
           its approach and in response to the publication of methodology by the
           European Commission.
         </p>
-        <p>
+        <p className="flex items-end">
           Please refer to the website for metrics we consider relevant to
           Agoda.com’s business.
         </p>
